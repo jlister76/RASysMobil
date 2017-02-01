956e03b86b6de13152d95cfab187d50c079a767b
John <jlister76@gmail.com>
2017-01-31 23:15:22 -0600

    Azure

--- common/models/risk-assessment.js
+++ common/models/risk-assessment.js
@@ -4,7 +4,7 @@ var loopback = require("loopback");
 var app = require('../../server/server');
 var path = require('path');
 var moment = require('moment');
-var config = require('config.env');
+var config = require('../../config.env.json');
 
 
 module.exports = function(Riskassessment) {
